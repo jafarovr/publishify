1b57166fe9dfcba501c42eb24bfadec3db335d6c
Ramin <rj@rjv.me>
2015-06-26 16:30:28 +0500

    folder browsing ok. symbolic links fixed. file showing is working.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var path    = require('path');
 var fm      = require('./file-manager');
+var fs      = require('fs');
+var mime    = require('mime');
 
-var default_path = 'public';
+var default_path = '/Users/rj/htdocs';
 
 var app = express();
 
@@ -11,30 +13,76 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  console.log(req.url);
-  // var err = new Error('Not Found');
-  // err.status = 404;
-  // next(err);
-  var path = new Object();
-  if (req.url == '/') {
-    path = default_path;
-  }
-  else {
-    path = default_path + req.url;
-  }
-
-  fm.listDir(path, function(response) {
-    console.log(path);
-    res.render('index', { files: response.files, path: req.url });
-  });
+  console.log('DEBUG: req.url: ' + req.url);
   
+  var path = new Object();
+
+  if (req.url == '/') path = default_path; 
+  else path = default_path + req.url;
+
+  fs.exists(path, function(exists) {
+    if (exists) {
+      if (fs.lstatSync(path).isDirectory() || fs.lstatSync(path).isSymbolicLink()) {
+        fm.listDir(path, function(err, response) {
+          console.log('DEBUG: path: ' + path);
+          if (err == null) {
+            res.render('index', { files: response.files, path: req.url });
+          }
+          else if(err == 'read err') {
+            var message = 'Directory/File read error!';
+            var status = 0;
+            res.render('error', { message: message, status: status });
+          }
+        });
+      }
+      else {
+        // console.log('file')
+        // res.end('hehe');
+        fm.serveStatic(path, function(err, file) {
+          if (!err) {
+            console.log('DEBUG: showFile: ' + path);
+            showFile(res, file);
+          }
+          else if (err == 'not found') {
+            var message = 'Not Found!';
+            var status = 404;
+            res.render('error', { message: message, status: status });
+          }
+          else if (err == 'read err') {
+            var message = 'Directory/File read error!';
+            var status = 0;
+            res.render('error', { message: message, status: status });
+          }
+        })
+      }
+    }
+    else {
+      var message = 'Not Found!';
+      var status = 404;
+      res.render('error', { message: message, status: status });
+    }
+  });
 });
+
+function showFile(response, file) {
+  var header = {'Content-Type' : mime.lookup(path.basename(file.path))};
+  console.log('DEBUG: showFile header: ' + mime.lookup(path.basename(file.path)));
+  response.writeHead(200, header);
+  response.end(file.contents);
+}
+
+function downloadFile(response, file) {
+  var header = {'Content-Type' : mime.lookup(path.basename(file.path)),
+                'Content-Disposition' : 'attachment;'};
+  console.log('DEBUG: showFile header: ' + mime.lookup(path.basename(file.path)));
+  response.writeHead(200, header);
+  response.end(file.contents);
+}
 
 app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + server.address().port);
+  console.log('node-file-manager listening on port ' + server.address().port);
 });
 
--- file-manager.js
+++ file-manager.js
@@ -1,37 +1,24 @@
 var http = require('http');
 var path = require('path');
 var fs   = require('fs');
-var mime = require('mime');
 
-function send404() {
-  var err = new Error('Not Found');
-  err.status = 404;
-}
-
-function sendFile(response, filePath, fileContents) {
-  var header = {'Content-Type' : mime.lookup(path.basename(filePath))};
-  response.writeHead(200, header);
-  response.end(fileContents);
-}
-
-function serveStatic(response, filePath) {
+exports.serveStatic = function (filePath, callback) {
   fs.exists(filePath, function(exists) {
     if (exists) {
-      if (fs.lstatSync(filePath).isDirectory()) {
-        listDir(response, filePath);
-      }
-      else
-        fs.readFile(filePath, function(err, data) {
-          if(err) {
-            send404();
-          }
-          else {
-            sendFile(response, filePath, data);
-          }
-        })
+      fs.readFile(filePath, function(err, data) {
+        if(err) {
+          callback('read err', null)
+        }
+        else {
+          var file = new Object();
+          file.path = filePath;
+          file.contents = data;
+          callback(null, file);
+        }
+      })
     }
     else {
-      send404();
+      callback('not found', null)
     }
   })
 }
@@ -40,19 +27,20 @@ exports.listDir = function(path, callback) {
   var response = new Object();
   fs.readdir(path, function(err, files) {
     if (err) {
-      send404();
+      console.log('ERROR: no such directory');
+      callback('read err', null);
     }
     else {
       response.path = path;
       response.files = [];
       files.forEach(function(f) {
-        file = new Object();
+        var file = new Object();
         file.name = f;
-        var stats = fs.statSync(path + "/" + f);
+        var stats = fs.statSync(path + '/' + f);
         file.size = stats.size;
         var d = new Date(Date.parse(stats.mtime));
         file.last_modified = d.toDateString();
-        if(fs.lstatSync(path + "/" + f).isDirectory()) {
+        if(fs.lstatSync(path + '/' + f).isDirectory() || fs.lstatSync(path + '/' + f).isSymbolicLink()) {
           file.type = 'folder';
         }
         else {
@@ -60,12 +48,7 @@ exports.listDir = function(path, callback) {
         }
         response.files.push(file);
       })
+      callback(null, response);
     }
-    callback(response);
   });
 }
-
-http.createServer(function(req, res) {
-  var path = 'files' + req.url;
-  serveStatic(res, path);
-}).listen(8000);
